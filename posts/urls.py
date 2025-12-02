81709724fe3aa329d7ff0ab6d53bf212e0a51069
Jeuan Mitchell <jeuan.mitchell@gmail.com>
2025-12-01 22:05:24 -0600

    Assignment 3 Update view filters, archive, draft view protection etc..

--- posts/urls.py
+++ posts/urls.py
@@ -5,9 +5,13 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='list'),
+    path('archived/', views.PostArchivedListView.as_view(), name='archived'),
+    path('drafts/', views.PostDraftListView.as_view(), name='drafts'),
     path('<int:pk>/', views.PostDetailView.as_view(), name='detail'),
     path('new/', views.PostCreateView.as_view(), name='new'),
     path('<int:pk>/edit/', views.PostUpdateView.as_view(), name='edit'),
     path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='delete'),
     path('<int:pk>/comment/', views.CommentCreateView.as_view(), name='add_comment'),
+    path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment_edit'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,27 +1,58 @@
 from django.views.generic import (
     ListView, DetailView, CreateView, DeleteView, UpdateView
 )
+from django.views.generic.edit import FormMixin
 from django.urls import reverse_lazy, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import get_object_or_404, redirect
 from .models import Post, Comment
+from .forms import CommentForm  
 
 
 class PostListView(ListView):
     model = Post
     template_name = 'posts/list.html'
     context_object_name = 'object_list'
+    
+    def get_queryset(self):
+        return Post.objects.filter(status__name='Published')
 
 
-class PostDetailView(DetailView):
+class PostDetailView(FormMixin, DetailView):
     model = Post
+    form_class = CommentForm
     template_name = 'posts/detail.html'
     context_object_name = 'post'
     
+    def get_queryset(self):
+        return Post.objects.filter(status__name='Published')
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = self.object.comments.all()
+        post = self.get_object()
+        context['comments'] = post.comments.all().order_by('-created_on')
+        if "form" not in context:
+            context['form'] = self.get_form()
         return context
+    
+    def get_success_url(self):
+        return reverse_lazy('posts:detail', kwargs={'pk': self.object.pk})
+    
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = self.object
+        form.save()
+        return super().form_valid(form)
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -43,6 +74,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     success_url = reverse_lazy('posts:list')
     context_object_name = 'post'
     login_url = 'login'
+    permission_denied_message = 'You do not have permission to edit this post.'
 
     def test_func(self):
         post = self.get_object()
@@ -55,6 +87,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     context_object_name = 'post'
     success_url = reverse_lazy('posts:list')
     login_url = 'login'
+    permission_denied_message = 'You do not have permission to delete this post.'
 
     def test_func(self):
         post = self.get_object()
@@ -74,4 +107,66 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     
     def get_success_url(self):
         return reverse('posts:detail', kwargs={'pk': self.kwargs['pk']})
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ['body']
+    template_name = 'posts/comment_edit.html'
+    context_object_name = 'comment'
+    login_url = 'login'
+    
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+    
+    def get_success_url(self):
+        return reverse('posts:detail', kwargs={'pk': self.object.post.pk})
+
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'posts/comment_delete.html'
+    context_object_name = 'comment'
+    login_url = 'login'
+    
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+    
+    def get_success_url(self):
+        return reverse('posts:detail', kwargs={'pk': self.object.post.pk})
+
+
+class PostArchivedListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'posts/archived.html'
+    context_object_name = 'archived_posts'
+    login_url = 'login'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Filter posts with Archived status that belong to the logged-in user
+        context['archived_posts'] = Post.objects.filter(
+            status__name='Archive',
+            author=self.request.user
+        ).order_by('-created_on')
+        return context
+
+
+class PostDraftListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'posts/drafts.html'
+    context_object_name = 'draft_posts'
+    login_url = 'login'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Filter posts with Draft status that belong to the logged-in user (author only)
+        context['draft_posts'] = Post.objects.filter(
+            status__name='Draft',
+            author=self.request.user
+        ).order_by('-created_on')
+        return context
+
 
